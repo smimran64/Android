4e411d1205f2237431c84bc2916fdcf8c8af6334
Md. Imran Mia <mr.t.imran11@gmail.com>
2025-10-29 16:30:39 +0600

    added

--- Musafir/app/src/main/java/com/example/myapplication/musafir/adapter/SurahAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication.musafir.adapter;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public class SurahAdapter extends RecyclerView.Adapter<SurahAdapter.ViewHolder> {
-}
